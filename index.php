ea3cb88a5ccceab5b75f7efb8f8499e09669a1d1
maximturchenko <maksarik1@yandex.ru>
2020-03-20 17:02:37 +0500

    Add tasks

---
+++ index.php
@@ -0,0 +1,128 @@
+<?php
+
+
+
+
+
+ /*
+ Tasks:
+
+
+1)Design efficient data structure for union-find:
+    ・ Количество объектов N может быть огромным.
+    ・ Количество операций М может быть огромным.
+    ・ Найти запросы и команды объединения могут быть смешаны.
+
+    Modeling the connections:
+    We assume "is connected to" is an equivalence relation:
+    ・Reflexive: p is connected to p.
+    ・Symmetric: if p is connected to q, then q is connected to p.
+    ・Transitive: if p is connected to q and q is connected to r,
+    then p is connected to r.
+
+    a)class UF : initialize union-find data structure with N objects (0 to N – 1)
+    b)function union(int p, int q) // add connection between p and q
+    c)boolean connected(int p, int q) // are p and q in the same component?
+    d)int find(int p) component identifier for p (0 to N – 1)
+    e)int count() number of components
+
+Dynamic-connectivity client
+・Read in number of objects N from standard input.
+・Repeat:
+– read in pair of integers from standard input
+– if they are not yet connected, connect them and print out pair
+
+public static void main(String[] args)
+{
+ int N = StdIn.readInt();
+ UF uf = new UF(N);
+ while (!StdIn.isEmpty())
+ {
+ int p = StdIn.readInt();
+ int q = StdIn.readInt();
+ if (!uf.connected(p, q))
+ {
+ uf.union(p, q);
+ StdOut.println(p + " " + q);
+ }
+ }
+}
+
+Quick-find
+Data structure.
+・Integer array id[] of length N.
+・Interpretation: p and q are connected iff they have the same id.
+
+Example union command
+ int pid = id[p];
+ int qid = id[q];
+ for (int i = 0; i < id.length; i++)
+ if (id[i] == pid) id[i] = qid;
+
+2)Quick-union [lazy approach]
+
+Data structure.
+・Integer array id[] of length N.
+・Interpretation: id[i] is parent of i.
+・Root of i is id[id[id[...id[i]...]]].
+
+Find. Check if p and q have the same root.
+
+Union. To merge components containing p and q,
+set the id of p's root to the id of q's root.
+
+public class QuickUnionUF
+{
+ private int[] id;
+ public QuickUnionUF(int N)
+ {
+ id = new int[N];
+ for (int i = 0; i < N; i++) id[i] = i;
+ }
+ private int root(int i)
+ {
+ while (i != id[i]) i = id[i];
+ return i;
+ }
+ public boolean connected(int p, int q)
+ {
+ return root(p) == root(q);
+ }
+ public void union(int p, int q)
+ {
+ int i = root(p);
+ int j = root(q);
+ id[i] = j;
+ }
+}
+
+3)Weighted quick-union.
+・Modify quick-union to avoid tall trees.
+・Keep track of size of each tree (number of objects).
+・Balance by linking root of smaller tree to root of larger tree
+
+4)Binary search
+
+Binary search. Compare key against middle entry.
+・Too small, go left.
+・Too big, go right.
+・Equal, found.
+
+
+ int lo = 0, hi = a.length-1;
+ while (lo <= hi)
+ {
+ int mid = lo + (hi - lo) / 2;
+ if (key < a[mid]) hi = mid - 1;
+ else if (key > a[mid]) lo = mid + 1;
+ else return mid;
+ }
+ return -1;
+
+Invariant. If key appears in the array a[], then a[lo] ≤ key ≤ a[hi].
+
+
+5)
+
+ 
+ */
